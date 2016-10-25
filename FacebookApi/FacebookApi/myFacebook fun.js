6c5831574b55f4fc59e8e4d05f2f8c593034f125
likaihe <1047561425@qq.com>
2016-10-25 13:45:05 +1300

    with Jquery

--- FacebookApi/FacebookApi/myFacebook fun.js
+++ FacebookApi/FacebookApi/myFacebook fun.js
@@ -1,57 +1,68 @@
-﻿window.fbAsyncInit = function () {
-    var accessToken;
-    FB.init({
+﻿
+$(document).ready(function () {
+    
+    $.ajaxSetup({ cache: true });
+    $.getScript('//connect.facebook.net/en_US/sdk.js', function () {
+        var accessToken;
+        FB.init({
 
-        //check the Id first!!
-        appId: '201650830263114',
-        xfbml: true,
-        version: 'v2.8'
-    });
-    FB.AppEvents.logPageView();
-    //check the connection
-    FB.getLoginStatus(function (response) {
-        if (response.status === 'connected') {
-            console.log('Logged in.');
-            //get accessToken
-            accessToken = response.authResponse.accessToken;
+            //check the Id first!!
+            appId: '201650830263114',
+            xfbml: true,
+            version: 'v2.8'
+        });
 
-            //*******************************facebook sdk call api***************************************//
-            //
-            //get user's infomation obj
-            //FB.api('/me', 'get', function (response) {
-            //    console.log(response);
-            //});
+        //???
+        FB.AppEvents.logPageView();
 
-            //********************************Jquery call api**************************************//
-            //
-            $.ajax({
-                url: "https://graph.facebook.com/me?access_token=" + accessToken,
-                method: "GET",
-                success: function () {
-                    alert("success");
-                }
-            });
+        //check the connection
+        FB.login(function (response) {
+            if (response.authResponse) {
+                //get accessToken
+                //accessToken = response.authResponse.accessToken;
 
+                //*******************************facebook sdk call api***************************************//
+                //
+                //get user's infomation obj
+                //FB.api('/me', 'get', function (response) {
+                //    console.log(response);
+                //});
 
-        }
-        else {
-            FB.login();
-            window.console.log("not");
-        }
-    });
+                //get uesr(log in) id, name, and picture
 
+                
+                FB.api('/me?fields=name,picture', 'get', function (response) {
+                    name = response.name;
+                    picture = response.picture.data.url;
+                    //do something
+                    dosomething(name, picture);
+                    
+                });
 
 
+                //********************************Jquery call api**************************************//
+                //note that accessToken is necessary 
+                //$.ajax({
+                //    url: "https://graph.facebook.com/me?access_token=" + accessToken,
+                //    method: "GET",
+                //    success: function () {
+                //        alert("success");
+                //    }
+                //});
+                
 
+            }
+            else {
+                console.log('User cancelled login or did not fully authorize.');
+            }
+        });//Finsihed FB.login
+        
+    });//Finished getScript
 
-};
+    //**********************other code*******************************//
+    dosomething = function (name, pirture) {
+        alert();
+    }
+   
 
-
-//facebook like and share
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) { return; }
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+});
